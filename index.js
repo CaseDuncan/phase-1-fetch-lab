bec18e415f7a99c0fffde6645ba3c8fe9fea9993
devdan <oluochdancun19@gmail.com>
2022-06-18 11:55:01 +0300

    commit tree 6976a4c101f47790956ba72c9667deb487171a5a

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  fetch("https://anapioficeandfire.com/api/books")
+  .then((res)=>res.json())
+  .then(data=>renderBooks(data))
+  // .then((data) =>{
+  //   const result = data.result
+  //   // console.log(result)
+  //   result.map((book)=>renderBooks(book))
+    
+  // });
   
 }
+fetchBooks();
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
